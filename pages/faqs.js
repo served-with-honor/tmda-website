4d18f8d4bb2e0f4727f8dcdc6cad0ad010053acb
Tim <timtran007@gmail.com>
2023-07-11 08:16:48 -0500

    adds filter feature

--- pages/faqs.js
+++ pages/faqs.js
@@ -1,28 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Accordion from '@mui/material/Accordion';
 import AccordionSummary from '@mui/material/AccordionSummary';
 import AccordionDetails from '@mui/material/AccordionDetails';
 import Box from '@mui/material/Box';
-import Grid from '@mui/material/Grid';
 import Container from '@mui/material/Container';
-// import Button from '@mui/material/Button';
-// import ButtonGroup from '@mui/material/ButtonGroup';
+import Button from '@mui/material/Button';
+import ButtonGroup from '@mui/material/ButtonGroup';
 import Typography from '@mui/material/Typography';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import Page from '../components/Page'
-import Link from '@mui/material/Link';
 
 export default function FAQsPage() {
+	const [selectedCategory, setSelectedCategory] = useState('')
+	
 	const items = [
 		{
 			question: 'Why is the sky blue?', 
 			answer: 'blue light is scattered more than the other colors because it travels in shorter, smaller waves',
-			topic: '',
+			topic: 'Nexus Service FAQ',
 		},
 		{
 			question: 'Is the Earth round of flat?', 
 			answer: 'round',
-			topic: '',
+			topic: 'Mental Health Eval FAQ',
 		},
 		{
 			question: 'Who is the apex predator of the sea?', 
@@ -30,8 +30,23 @@ export default function FAQsPage() {
 			topic: '',
 		},
 	]
-
+	
+	const [filteredItems, setFilteredItems] = useState(items)
+	
 	const topics = ["Nexus Service FAQ", "Mental Health Eval FAQ", "Telemedicine Eval FAQ", "Therapy FAQ"]
+
+	const selectFilter = (topic) => {
+		setSelectedCategory(topic)
+		if(topic === '') {
+			setFilteredItems(items)
+		}
+		else {
+			const filtered = items.filter(item => item.topic === topic)
+			setFilteredItems(filtered)
+		}
+	}
+
+	
 	return (
 		<Page title={'FAQs'}>
 			{/* <Box sx={{ paddingTop: 20, paddingBottom: 20 , position: 'relative', }}>
@@ -45,12 +60,57 @@ export default function FAQsPage() {
 			</Box> */}
 
 			<Box sx={{ backgroundColor: 'secondary.100', paddingTop: 20, paddingBottom: 20, }}>
-				<Container sx={{alignItems: "center" }}>
-					<Typography variant='sectionHeading' component='h2' sx={{ marginBottom: 10, marginX: 'auto' }}>FAQ</Typography>
+				{selectedCategory === '' ?
+					<Container sx={{alignItems: "center" }}>
+						<Typography 
+							variant='sectionHeading' 
+							component='h2' 
+							sx={{ 
+								marginBottom: 10, 
+								marginX: 'auto', 
+							}}
+						>
+							FAQ
+						</Typography>
+						{items.map( (item, index) => 
+							(
+								<Accordion key={index}>
+									<AccordionSummary
+										expandIcon={<ExpandMoreIcon />}
+										aria-controls="panel1a-content"
+										id="panel1a-header"
+										sx={{ margin: 2}}
+										>
+										<Typography variant='subtitle1'>{item.question}</Typography>
+									</AccordionSummary>
+									<AccordionDetails sx={{ paddingLeft: 4}}>
+										<Typography variant='body1'>{item.answer}</Typography>
+									</AccordionDetails>
+								</Accordion>
+							))}
 					
-					{items.map(item => 
+						<div style={{ paddingTop: 50, display: "flex", justifyContent: "center", alignItems: "center" }}>
+							{topics.map( (topic, index) => 
+								<Button key={index} onClick={() => selectFilter(topic)} variant='text' size='small' color="inherit">
+									{topic}
+								</Button>	
+							)}
+						</div>
+					</Container> :
+					<Container sx={{alignItems: "center" }}>
+					<Typography 
+						variant='sectionHeading' 
+						component='h2' 
+						sx={{ 
+							marginBottom: 10, 
+							marginX: 'auto', 
+						}}
+					>
+						FAQ
+					</Typography>
+					{filteredItems.map((item, index) =>
 						(
-							<Accordion key={item}>
+							<Accordion key={index}>
 								<AccordionSummary
 									expandIcon={<ExpandMoreIcon />}
 									aria-controls="panel1a-content"
@@ -66,13 +126,14 @@ export default function FAQsPage() {
 						))}
 				
 					<div style={{ paddingTop: 50, display: "flex", justifyContent: "center", alignItems: "center" }}>
-						{topics.map(topic => 
-							<Link href="" color="inherit" sx={{ padding: 1, alignContent: "center"}}>
+						{topics.map( (topic, index) => 
+							<Button key={index} onClick={() => selectFilter(topic)} variant='text' size='small' color="inherit">
 								{topic}
-							</Link>	
+							</Button>	
 						)}
 					</div>
 				</Container>
+				}
 			</Box>
   		</Page>
   )
