e2e6cc56d2217ea6a27d766e1453035cf9304cdc
Tim <timtran007@gmail.com>
2023-07-13 10:23:15 -0500

    destructures params from context in getServerSideProps and passes variable

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -3,19 +3,22 @@ import { getPost } from '../../lib/api'
 import Page from '../../components/Page'
 
 export default function Post({ post }) {
+
 	return (
 		<Page title={'News & Updates'}>
 			<Box marginTop={10}>
 				<Container>
 					<Typography variant={'h1'}>{post.title}</Typography>
+
 				</Container>
 			</Box>
   	</Page>
   )
 }
 
-export const getServerSideProps = async () => {
-	const response = await getPost()
+export const getServerSideProps = async ({ params }) => {
+	const idSlug = params.slug
+	const response = await getPost(idSlug)
 	const post = response.post
 
 	return { props: { post } }
