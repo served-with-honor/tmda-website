5cec794b896d94d2fcf784c69b040c4a3c525c6f
Tim <timtran007@gmail.com>
2023-07-14 08:16:46 -0500

    adds featured image and post content

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -3,12 +3,26 @@ import { getPost } from '../../lib/api'
 import Page from '../../components/Page'
 
 export default function Post({ post }) {
+	debugger
 
 	return (
 		<Page title={'News & Updates'}>
-			<Box marginTop={10}>
+			<Box sx={{py: 25}}>
 				<Container>
 					<Typography variant={'h1'}>{post.title}</Typography>
+					<Box
+						component="img"
+						sx={{
+							maxWidthidth: 100,
+							py: 5,
+						}}
+						alt={post.featuredImage.node.altText}
+						src={post.featuredImage.node.mediaItemUrl}
+					>
+
+					</Box>
+					<div dangerouslySetInnerHTML={{__html: post.content}}>
+					</div>
 
 				</Container>
 			</Box>
