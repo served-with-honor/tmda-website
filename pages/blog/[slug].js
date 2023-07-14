7f7f86174884f3a8ee3956c2e351d82333b56de2
Tim <timtran007@gmail.com>
2023-07-14 12:03:04 -0500

    adds date written, date modified and post tags

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -1,29 +1,45 @@
-import { Box, Card, CardContent, Container, Stack, Typography } from '@mui/material';
+import { Box, Card, CardContent, Chip, Container, Stack, Typography } from '@mui/material';
 import { getPost } from '../../lib/api'
 import Page from '../../components/Page'
 
 export default function Post({ post }) {
+
+	const dateWritten = new Date(post.date).toLocaleDateString()
+
+	const dateModified = new Date(post.modified).toLocaleDateString()
+
+	const postTags = post.tags.nodes
+
 	debugger
 
 	return (
 		<Page title={'News & Updates'}>
-			<Box sx={{py: 25}}>
+			<Box sx={{py: 20}}>
 				<Container>
 					<Typography variant={'h1'}>{post.title}</Typography>
+					<Typography variant={'body1'} sx={{py: 4}}>By {post.author.node.name} - {dateWritten} </Typography>
+					{dateModified !== dateWritten ?
+						<Typography variant={'body2'}>Modified on {dateModified}</Typography> :
+						<div></div>
+					}
 					<Box
 						component="img"
 						sx={{
 							maxWidthidth: 100,
-							py: 5,
+							py: 4,
 						}}
-						alt={post.featuredImage.node.altText}
+						alt={post.featuredImage.node.altText}	
 						src={post.featuredImage.node.mediaItemUrl}
 					>
-
 					</Box>
-					<div dangerouslySetInnerHTML={{__html: post.content}}>
-					</div>
-
+					<div dangerouslySetInnerHTML={{__html: post.content}}></div>
+					<Box sx={{pt: 10}}>
+					{postTags.map((tag, index) => {
+						return(
+							<Chip key={`${tag.name}-${index}`} label={tag.name}></Chip>
+						)}
+					)}
+					</Box>
 				</Container>
 			</Box>
   	</Page>
