75b235448fd361ffd95f58c56fda9e322311d48a
Lee Wise <theleewise@users.noreply.github.com>
2023-07-20 14:37:46 -0500

    Pages/blog (#3)
    * creates async function for getPost for hardcoded slug
    
    * adds getServerSideProps and imports getPost
    
    * switches hardcoded slug to variable
    
    * destructures params from context in getServerSideProps and passes variable
    
    * adds more data to query from GraphiQL
    
    * adds featured image and post content
    
    * adds date written, date modified and post tags
    
    * adds author avatar image url to API request
    
    * adds hero section with avatar
    
    * fixes mobile responsive with wordWrap
    
    * updates background color layover to black with opacity of .65
    
    * updates text color to white for modified date
    
    * Fix broken var for slug
    
    * refactor blog detail page props
    
    * Improve blog hero bg
    
    * Remove unnecessary grid component
    
    * Remove unused import
    
    * Move tags to hero
    
    * Make blog tags clickable
    
    * add tag colors on blog post
    
    * change tags wrapper to stack component
    
    * improve tags key
    
    * reformating use destructuring and props on new lines
    
    * move modified date next to avatar under publish date
    
    * Remove unnecessary div
    
    * only show tags if post has them
    
    * small spacing change
    
    * remove unneeded var
    
    ---------
    
    Co-authored-by: Tim <timtran007@gmail.com>

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -1,14 +1,73 @@
-import { Box, Card, CardContent, Container, Stack, Typography } from '@mui/material';
+import { useRouter } from 'next/router';
+import Box from '@mui/material/Box'
+import Chip from '@mui/material/Chip'
+import Container from '@mui/material/Container'
+import Stack from '@mui/material/Stack'
+import Typography from '@mui/material/Typography'
+import Avatar from '@mui/material/Avatar'
+import { getPost } from '../../lib/api'
 import Page from '../../components/Page'
+import settings from '../../src/siteSettings';
 
 export default function Post({ post }) {
+	const { author, tags, title, content, featuredImage, date, modifed } = post;
+	const publishDate = new Date(date).toLocaleDateString();
+	const modifedDate = modifed ? new Date(modifed).toLocaleDateString() : null;
+
+	const router = useRouter();
+  const handleTagClick = (slug) => router.push(`/blog?tag=${slug}`);
+
 	return (
-		<Page title={'News & Updates'}>
-			<Box marginTop={10}>
+		<Page title={title}>
+			<Box sx={{
+				pt: 15,
+				pb: 10,
+				width: '100%',
+				backgroundColor: 'primary.100',
+				backgroundImage: `url(${featuredImage})`,
+				backgroundSize: "cover", 
+				backgroundPosition: 'center',
+				backgroundBlendMode: 'overlay',
+			}}
+			>
 				<Container>
-					<Typography variant={'h1'}>Heyo</Typography>
+					<Typography variant='h1' color='secondary' gutterBottom>{title}</Typography>
+					<Stack direction="row" spacing={2} sx={{ alignItems: 'center' }}>
+						<Avatar alt={author.name} src={author.image} sx={{ width: 48, height: 48 }}></Avatar>
+						<Box>
+							<Typography variant={'body1'}>By {author.name} - {publishDate}</Typography>
+							{modifedDate && modifedDate !== publishDate ? (
+								<Typography sx={{ py: 1 }} variant={'body2'}>Last Modified on {modifedDate}</Typography>
+							) : null}
+						</Box>
+					</Stack>
+					{tags ? (
+						<Stack direction="row" spacing={1} sx={{ mt: 5 }}>
+							{tags.map(({ slug, name }) => {
+								const color = settings.articleTagColors[slug];
+								return (
+									<Chip
+										key={`post-tag-${slug}`}
+										label={name}
+										onClick={() => handleTagClick(slug)}
+										sx={color ? { color: '#fff', backgroundColor: color } : {}}
+									/>
+								);
+							})}
+						</Stack>
+					) : null}
+				</Container>
+			</Box>
+			<Box sx={{ my: 10 }}>
+				<Container>
+					<Box sx={{ wordWrap: 'break-word' }} dangerouslySetInnerHTML={{__html: content}} />
 				</Container>
 			</Box>
   	</Page>
   )
+}
+
+export const getServerSideProps = async ({ params }) => {
+	const { post } = await getPost(params.slug);
+	return { props: { post } }
 }
