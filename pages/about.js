8f80bdf9c817b431395a4f4ca55a33bd946e7083
Lee Wise <theleewise@users.noreply.github.com>
2023-08-10 11:43:59 -0500

    Feature/about page/serve section (#7)
    * adds the Serve section through mui tabs component
    
    * adds doctor image for the Serve Tab
    
    * changes layout to have image in md media query to be slightly above border of Serve Tabs
    
    * adds padding
    
    * hides image when it has media query of sm or smaller
    
    * removes image in serve-section from tablet and phone
    
    * imporve styles
    
    * move content to page props
    
    * Move to seperate component
    
    * improve spacing
    
    * remove ununnecessary props
    
    * fixing tabs image
    
    * refactor - move compenent within parent
    
    * remove unnecessary imports
    
    * improve proptypes
    
    * improve naming
    
    * improve a11y
    
    * fix proptype error
    
    * imporve responsive
    
    ---------
    
    Co-authored-by: Tim <timtran007@gmail.com>

--- pages/about.js
+++ pages/about.js
@@ -15,6 +15,7 @@ import MapAnim from '../components/MapAnim'
 import { getTeamMembers } from '../lib'
 import siteSettings from '../src/siteSettings';
 import Counter from '../components/Counter'
+import CustomTabs from '../components/CustomTabs'
 import Directory from '../components/Directory'
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
@@ -24,7 +25,7 @@ import Dialog from '@mui/material/Dialog';
 import BookingWidget from '../components/BookingWidget'
 import texture01 from '../public/texture-01.jpg'
 
-export default function AboutPage({ teamMembers, providers }) {
+export default function AboutPage({ teamMembers, providers, serveTabs }) {
 	const [isBookingDialogOpen, setIsBookingDialogOpen] = useState(false);
 	const heroRef = useRef(null);
 	const sliderSettings = {
@@ -91,7 +92,7 @@ export default function AboutPage({ teamMembers, providers }) {
 			</Box>
 
 			{/* SECTION */}
-			<Box sx={{ backgroundColor: 'secondary.100', paddingY: 20 }}>
+			<Box sx={{ backgroundColor: 'secondary.100', paddingTop: 20, paddingBottom: 30  }}>
 				<Container>
 					<Typography variant='sectionHeading' component='h2' sx={{ marginBottom: 10, maxWidth: 'sm', marginX: 'auto' }}>We Are Committed To Serving Those Who Served</Typography>
 					<Grid container spacing={3}>
@@ -113,12 +114,12 @@ export default function AboutPage({ teamMembers, providers }) {
 				</Container>
 			</Box>
 			
-			{/* SECTION */}
-			<Box sx={{ paddingY: 20 }}>
-				<Container>
-					<Typography variant='sectionHeading' component='h2' sx={{ marginBottom: 10 }}>STUFF HERE</Typography>
+			{/* SECTION - SERVE TABS */}
+			{serveTabs ? (
+				<Container sx={{ mt: -4, }}>
+					<CustomTabs items={serveTabs} name="Serve Tabs" />
 				</Container>
-			</Box>
+			) : null}
 
 			{/* SECTION */}
 			<Box sx={{ paddingY: 20 }}>
@@ -224,6 +225,23 @@ export const getServerSideProps = async () => {
 		{ name: 'Gracelynn Barnes', position: 'Proin Consectetur Neque', },
 		{ name: 'James Edwards', position: 'Ut Malesuada Dolor', },
 		{ name: 'Finn Butler', position: 'Etiam Hendrerit Turpis', },
+	];
+	const serveTabs = [
+		{
+			title: 'Who We Serve',
+			heading: 'Who',
+			body: 'We serve members of the veteran community who are seeking to apply for, or increase, the VA disability benefits they’ve earned for their honorable service.',
+		},
+		{
+			title: 'How We Serve',
+			heading: 'How',
+			body: 'High-quality medical evidence helps veterans win claims! From DBQs and Nexus Letters to Psych Evals and Telemedicine Evaluations, we make it easier than ever for veterans connect with a licensed provider through our HIPAA compliant telemedicine platform - anytime, anywhere.',
+		},
+		{
+			title: 'Why We Serve',
+			heading: 'Why',
+			body: 'No veteran deserves to be denied or underrated for disability benefits. When veterans submit medical evidence with their VA disability claims, they are more likely to win that claim. We are here to help you on your path to wellbeing.',
+		},
 	]
-	return { props: { teamMembers, providers } };
+	return { props: { teamMembers, providers, serveTabs } };
 }
