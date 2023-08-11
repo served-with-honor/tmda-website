d3298728d131820cd939171fb9daea71dc3100f9
Tim <timtran007@gmail.com>
2023-08-11 16:17:04 -0500

    adds layout for Custom Action Section

---
+++ components/Custom3ActionSection.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import PropTypes from 'prop-types';
+import Grid from "@mui/system/Grid";
+
+Custom3ActionSection.propTypes = {
+    name: PropTypes.string,
+    items: PropTypes.arrayOf(PropTypes.shape({
+        icon: PropTypes.string.isRequired,
+        heading: PropTypes.string,
+        description: PropTypes.string,
+    })).isRequired,
+};
+
+export default function Custom3ActionSection({ items, name = 'Custom 3 Action Section'}) {
+    return (
+        <Grid>
+            
+        </Grid>
+    )
+}
