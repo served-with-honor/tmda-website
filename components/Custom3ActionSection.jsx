e0b2785e521bc74ff3b06e90a3be5fe0e0ebc9a6
Tim <timtran007@gmail.com>
2023-08-14 15:02:57 -0500

    adds 3 Box CTA components as functional component

--- components/Custom3ActionSection.jsx
+++ components/Custom3ActionSection.jsx
@@ -1,6 +1,13 @@
 import { useState } from "react";
 import PropTypes from 'prop-types';
-import Grid from "@mui/system/Grid";
+import Grid from '@mui/material/Grid';
+import Stack from "@mui/material/Stack";
+import Box from "@mui/material/Box";
+import Image from "next/image";
+import Paper from "@mui/material/Paper";
+import Typography from "@mui/material/Typography";
+import Button from '@mui/material/Button';
+
 
 Custom3ActionSection.propTypes = {
     name: PropTypes.string,
@@ -14,9 +21,29 @@ Custom3ActionSection.propTypes = {
 };
 
 export default function Custom3ActionSection({ items, name = 'Custom 3 Action Section'}) {
+
+    function handleCTAClick(urlLink){
+        debugger
+    }
+
     return (
-        <Grid >
-            
+        <Grid container sx={{minWidth: '100%'}}>
+            {items.map(({icon, heading, description, urlLink, buttonLabel}, index) => {
+                return(
+                    <Paper onClick={() => handleCTAClick(urlLink)} sx={{m: 5, width: '25%', minHeight: '300px', borderRadius: 0, backgroundColor: 'secondary.100'}}>
+                        <Stack sx={{alignItems: 'center', pt: 5}} direction='column'>
+                            <Image 
+                                src={icon}
+                                width={60}
+                                height={60}
+                            />
+                            <Typography color='secondary' variant='h6' sx={{pt:2}}>{heading}</Typography>
+                            <Typography color='secondary' variant='text' sx={{p:2}}>{description}</Typography>
+                            <Button variant={'outlined'} sx={{mb: 5}}>{buttonLabel}</Button>
+                        </Stack>
+                    </Paper>
+                )
+            })}
         </Grid>
     )
 }
