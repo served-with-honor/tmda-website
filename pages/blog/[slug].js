24fc2d2004c35a05ec4b8303208d73f70c3c4a75
Lee Wise <theleewise@users.noreply.github.com>
2023-09-05 14:27:59 -0500

    render blog content as mui components

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -1,17 +1,26 @@
 import { useRouter } from 'next/router';
+import parse, { domToReact } from 'html-react-parser';
 import Box from '@mui/material/Box'
 import Chip from '@mui/material/Chip'
 import Container from '@mui/material/Container'
+import Divider from '@mui/material/Divider';
 import Stack from '@mui/material/Stack'
 import Grid from '@mui/material/Grid'
 import Typography from '@mui/material/Typography'
 import Avatar from '@mui/material/Avatar'
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import AddIcon from '@mui/icons-material/Add';
 import { getPost } from '../../lib/api'
 import Page from '../../components/Page'
+import Link from '../../src/Link';
 import settings from '../../src/siteSettings';
 
 export default function Post({ post }) {
 	const { author, tags, title, content, featuredImage, date, modifed } = post;
+	const contentComponents = parse(content, { replace: replaceContent });
 	const publishDate = new Date(date).toLocaleDateString();
 	const modifedDate = modifed ? new Date(modifed).toLocaleDateString() : null;
 
@@ -62,7 +71,7 @@ export default function Post({ post }) {
 			</Box>
 			<Box sx={{ my: 10 }}>
 				<Container>
-					<Box sx={{ wordWrap: 'break-word' }} dangerouslySetInnerHTML={{__html: content}} />
+					{contentComponents}
 				</Container>
 			</Box>
   	</Page>
@@ -72,4 +81,45 @@ export default function Post({ post }) {
 export const getServerSideProps = async ({ params }) => {
 	const { post } = await getPost(params.slug);
 	return { props: { post } }
+}
+
+const replaceContent = element => {
+
+	const renderElementContents = (element) => {
+		if (element.children) return element.children.map(item => {
+			const { children, name, attribs, type } = item;
+			if (type === 'text') return item.data;
+			if (children) {
+				if (name === 'a') return <Link {...attribs}>{domToReact(children)}</Link>
+				return domToReact(children);
+			}
+			
+			return item;
+		});
+
+		return domToReact(element);
+	}
+
+	const renderUnorderedList = (element) => (
+		<List disablePadding>
+			{element.children.map(item => item.type === 'tag' ? (
+				<ListItem dense>
+					<ListItemIcon sx={{ minWidth: 0, mr: 2 }}><AddIcon sx={{ color: 'primary.main' }} /></ListItemIcon>
+					<ListItemText>{renderElementContents(item)}</ListItemText>
+				</ListItem>
+			) : null)}
+		</List>
+	);
+
+	if (element.name === 'p') return <Typography variant='body1' my={3}>{renderElementContents(element)}</Typography>
+	if (element.name === 'h1') return <Typography variant='h4' component='h2' color='secondary.main' my={6}>{domToReact(element.children)}</Typography>
+	if (element.name === 'h2') return <Typography variant='h4' component='h2' color='secondary.main' my={6}>{domToReact(element.children)}</Typography>
+	if (element.name === 'h3') return <Typography variant='h5' component='h3' color='secondary.main' my={5}>{domToReact(element.children)}</Typography>
+	if (element.name === 'h4') return <Typography variant='h6' component='h4' color='secondary.main' my={4}>{domToReact(element.children)}</Typography>
+	if (element.name === 'h5') return <Typography variant='h6' component='h5' color='secondary.main' my={3}>{domToReact(element.children)}</Typography>
+	if (element.name === 'h6') return <Typography variant='h6' component='h6' color='secondary.main' my={2}>{domToReact(element.children)}</Typography>
+	if (element.name === 'ul') return renderUnorderedList(element);
+	if (element.name === 'hr') return <Divider />;
+	
+	return element;
 }
