5bbc3004df4730e8f261130a6f21e84ddfb51f65
Lee Wise <theleewise@users.noreply.github.com>
2023-09-19 15:15:15 -0500

    extract components

---
+++ components/BlogHero.jsx
@@ -0,0 +1,74 @@
+import { useRouter } from 'next/router';
+import Box from '@mui/material/Box'
+import Chip from '@mui/material/Chip'
+import Container from '@mui/material/Container'
+import Stack from '@mui/material/Stack'
+import Grid from '@mui/material/Grid'
+import Typography from '@mui/material/Typography'
+import Avatar from '@mui/material/Avatar'
+import settings from '../src/siteSettings';
+import FancyTitle from './FancyTitle';
+
+export default function BlogHero({ title, author, date, modifed, categories, featuredImage }) {
+	const router = useRouter();
+  const handleCategoryClick = (slug) => router.push(`/blog?category=${slug}`);
+	
+	const publishDate = date ? new Date(date).toLocaleDateString() : null;
+	const modifedDate = modifed ? new Date(modifed).toLocaleDateString() : null;
+
+	return (
+		<Box sx={{
+			pt: 15,
+			pb: 10,
+			width: '100%',
+			backgroundColor: 'primary.100',
+			backgroundImage: `url(${featuredImage})`,
+			backgroundSize: "cover",
+			backgroundPosition: 'center',
+			backgroundBlendMode: 'overlay',
+		}}
+		>
+			<Container>
+				<Typography variant='h1' color='secondary' gutterBottom>
+					<FancyTitle title={title} styles={{ color: 'primary.main', display: 'block', fontSize: '0.5em', fontWeight: 400, lineHeight: '1em' }} />
+				</Typography>
+
+				{author || publishDate ? (
+					<Stack direction="row" spacing={2} sx={{ alignItems: 'center' }}>
+						{author ? <>
+							<Avatar alt={author.name} src={author.image} sx={{ width: 48, height: 48 }}></Avatar>
+							<Box>
+								<Typography variant={'body1'}>By {author.name}</Typography>
+							</Box>
+						</> : null}
+						{publishDate ? (
+							<Box>
+								<Typography variant={'body1'}>{publishDate}</Typography>
+								{modifedDate && modifedDate !== publishDate ? (
+									<Typography sx={{ py: 1 }} variant={'body2'}>Last Modified on {modifedDate}</Typography>
+								) : null}
+							</Box>
+						) : null}
+					</Stack>
+				) : null}
+
+				{categories ? (
+					<Grid container spacing={1} sx={{ mt: 5 }}>
+						{categories.map(({ slug, name }) => {
+							const color = settings.articleCategoryColors[slug];
+							return (
+								<Grid item key={`post-category-${slug}`}>
+									<Chip
+										label={name}
+										onClick={() => handleCategoryClick(slug)}
+										sx={color ? { color: '#fff', backgroundColor: color } : {}}
+									/>
+								</Grid>
+							);
+						})}
+					</Grid>
+				) : null}
+			</Container>
+		</Box>
+	);
+}
--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -1,90 +1,19 @@
-import { useRouter } from 'next/router';
 import parse from 'html-react-parser';
 import Box from '@mui/material/Box'
-import Chip from '@mui/material/Chip'
 import Container from '@mui/material/Container'
-import Stack from '@mui/material/Stack'
-import Grid from '@mui/material/Grid'
-import Typography from '@mui/material/Typography'
-import Avatar from '@mui/material/Avatar'
 import { getPost } from '../../lib/api'
 import Page from '../../components/Page'
-import { splitTitle } from '../../src/utils';
-import settings from '../../src/siteSettings';
+import BlogHero from '../../components/BlogHero'
 import { replaceContent } from '../../src/WPBlocks';
 
 export default function Post({ post }) {
 	const { author, categories, title, content, featuredImage, date, modifed } = post;
 	const contentComponents = parse(content, { replace: replaceContent });
-	const publishDate = new Date(date).toLocaleDateString();
-	const modifedDate = modifed ? new Date(modifed).toLocaleDateString() : null;
-
-	const router = useRouter();
-  const handleCategoryClick = (slug) => router.push(`/blog?category=${slug}`);
-
-	const FancyTitle = () => {
-		const { primaryText, preText, postText } = splitTitle(title);
-		const secondaryStyles = { display: 'block', fontSize: '0.5em', fontWeight: 400, lineHeight: '1em' };
-
-		return <>
-			{preText && (
-				<Box component='span' color='primary.main' sx={secondaryStyles}>
-					{preText}
-				</Box>
-			)}
-			{primaryText}
-			{postText && (
-				<Box component='span' color='primary.main' sx={secondaryStyles}>
-					{postText}
-				</Box>
-			)}
-		</>
-	}
-
+	
 	return (
 		<Page title={title}>
-			<Box sx={{
-				pt: 15,
-				pb: 10,
-				width: '100%',
-				backgroundColor: 'primary.100',
-				backgroundImage: `url(${featuredImage})`,
-				backgroundSize: "cover", 
-				backgroundPosition: 'center',
-				backgroundBlendMode: 'overlay',
-			}}
-			>
-				<Container>
-					<Typography variant='h1' color='secondary' gutterBottom>
-						<FancyTitle />
-					</Typography>
-					<Stack direction="row" spacing={2} sx={{ alignItems: 'center' }}>
-						<Avatar alt={author.name} src={author.image} sx={{ width: 48, height: 48 }}></Avatar>
-						<Box>
-							<Typography variant={'body1'}>By {author.name} - {publishDate}</Typography>
-							{modifedDate && modifedDate !== publishDate ? (
-								<Typography sx={{ py: 1 }} variant={'body2'}>Last Modified on {modifedDate}</Typography>
-							) : null}
-						</Box>
-					</Stack>
-					{categories ? (
-						<Grid container spacing={1} sx={{ mt: 5 }}>
-							{categories.map(({ slug, name }) => {
-								const color = settings.articleCategoryColors[slug];
-								return (
-									<Grid item key={`post-category-${slug}`}>
-										<Chip
-											label={name}
-											onClick={() => handleCategoryClick(slug)}
-											sx={color ? { color: '#fff', backgroundColor: color } : {}}
-										/>
-									</Grid>
-								);
-							})}
-						</Grid>
-					) : null}
-				</Container>
-			</Box>
+			<BlogHero {...{ title, author, date, modifed, categories, featuredImage }} />
+			
 			<Box sx={{ my: 10 }}>
 				<Container>
 					{contentComponents}
