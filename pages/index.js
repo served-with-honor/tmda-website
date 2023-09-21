2b6011a331c16ff7cd327ee36f8e8e09f6c02a4f
Tim <timtran007@gmail.com>
2023-09-21 16:09:11 -0500

    completes front page blog section

--- pages/index.js
+++ pages/index.js
@@ -175,12 +175,12 @@ export default function Home({ faqs, testimonials }) {
 
 			<Box paddingY={12}>
 				<Container>
-					<Typography variant={'sectionHeading'} component={'h2'} sx={{ mb: 8 }}>Check Out These Free Resources!<br />Find out more about _____.</Typography>
+					<Typography variant={'sectionHeading'} component={'h2'} sx={{ mb: 8 }}>Free Resource for Veterans</Typography>
 					<Box sx={{ mb: 8 }}>
 						<LatestPosts />
 					</Box>
 					<Box align={'center'}>
-						<Button variant={'contained'} href={'/blog'}>Find more free resources</Button>
+						<Button variant={'contained'} href={'/blog'}>More free resources</Button>
 					</Box>
 				</Container>
 			</Box>
