eeae09f6761575516a772261d163fee2f46289c0
Tim <timtran007@gmail.com>
2023-09-22 11:55:10 -0500

    adds Chip only to Directory component if there are categories

--- components/Directory.jsx
+++ components/Directory.jsx
@@ -8,6 +8,8 @@ import Tabs from '@mui/material/Tabs';
 import { motion } from 'framer-motion'
 import { slugify } from '../src/utils';
 import defaultProfile from '../public/default-profile.png'
+import Chip from '@mui/material/Chip';
+import { blue, green, red } from '@mui/material/colors'
 
 export default function Directory({ items }) {
   const [value, setValue] = useState(0);
@@ -21,7 +23,7 @@ export default function Directory({ items }) {
       <TabPanel key={`directory-panel-${index}`} value={value} index={index}>
         {people ? (
           <Grid container spacing={5} sx={{ justifyContent: 'center' }}>
-            {people.map(({ name, position, image }, index) => {
+            {people.map(({ name, position, image, category }, index) => {
               return (
                 <Grid key={`directory-team-member-${slugify(name)}`} item xs={12} sm={6} md={3} lg={2}>
                   <motion.div
@@ -30,7 +32,7 @@ export default function Directory({ items }) {
                     whileInView={{ opacity: 1, y: 0 }}
                     transition={{ delay: 0.05 * index }}
                   >
-                    <Person name={name} image={image} position={position} />
+                    <Person name={name} image={image} position={position} category={category}/>
                   </motion.div>
                 </Grid>
               );
@@ -77,12 +79,26 @@ const TabPanel = ({ children, value, index, ...other }) => (
   </div>
 );
 
-const Person = ({ name, position, image }) => {
+const Person = ({ name, position, image, category }) => {
   const imageUrl = image && Array.isArray(image) ? image[0] : image ? image : defaultProfile.src;
   const srcset = image && Array.isArray(image) && image.length > 0 ? image.join(', ') : null;
+  function checkColor(c) {
+    if (c === 'Psych'){
+      return blue[300]
+    } else if (c === 'Nexus') {
+      return red[400]
+    } else if (c === 'Telemedicine Eval'){
+      return green[400]
+    }
+  }
   return <>
     <Avatar srcSet={srcset} src={imageUrl} alt={`${name} profile photo`} sx={{ width: 150, height: 150, marginBottom: 3, mx: 'auto' }} />
     <Typography variant='h6' component='p'>{name}</Typography>
     <Typography variant='body2'>{position}</Typography>
+    {category && category.map(c => {
+      return(
+        <Chip key={`${name}-${c}`}label={c} sx={c && {color: '#fff', backgroundColor: checkColor(c)}}/>
+      )
+    })}
   </>
 };
