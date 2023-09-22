31d7e3ad35e672980bd6cb1a5ded4318a2de15be
Tim <timtran007@gmail.com>
2023-09-22 08:32:01 -0500

    adds favicon

--- components/Page.jsx
+++ components/Page.jsx
@@ -32,6 +32,13 @@ export default function Page({ title, description, children, darkHeader }) {
         <meta property="og:description" content={pageDescription} />
         <meta property="og:locale" content="en_US" />
         <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+        <link rel="manifest" href="/site.webmanifest"/>
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#74c8b4"/>
+        <meta name="msapplication-TileColor" content="#74c8b4"/>
+        <meta name="theme-color" content="#ffffff"/>
         {settings.googleAnalyticsId !== undefined ? (<>
           <script src={`https://www.googletagmanager.com/gtag/js?id=${settings.googleAnalyticsId}`} />
           <script id="google-analytics">
