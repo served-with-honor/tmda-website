5f0bf8be3069eefc18fd1805851e1bab9fbaf423
Tim <timtran007@gmail.com>
2023-10-05 15:53:18 -0500

    refactors to single loop, adds author and categories, logo path, imports siteSettings

--- utils/generateRSSFeed.js
+++ utils/generateRSSFeed.js
@@ -1,37 +1,43 @@
 import fs from 'fs';
 import RSS from 'rss';
 import { getPosts } from '../lib/api';
+import settings from '../src/siteSettings'
 
 export default async function generateRssFeed() {
-    const site_url = 'localhost:3000';
+    const site_url = settings.url
     
     const response = await getPosts();
-    const allPosts = response.posts.nodes.map((post) => ({
-        title: post.title,
-        excerpt: post.excerpt,
-        slug: post.slug,
-        date: post.date,
-        image: post.featuredImage?.node?.mediaItemUrl,
-        categories: post.categories.edges?.map(a => a.node),
-    }));
+    // const allPosts = response.posts.nodes.map((post) => ({
+    //     title: post.title,
+    //     excerpt: post.excerpt,
+    //     slug: post.slug,
+    //     date: post.date,
+    //     image: post.featuredImage?.node?.mediaItemUrl,
+    //     categories: post.categories.edges?.map(a => a.node),
+    //     author: post.author.node.name,
+    // }));
 
     const feedOptions = {
-        title: 'Blog posts | RSS Feed',
-        description: 'Checkout the blog posts',
+        title: settings.defaultPageTitle,
+        description: settings.defaultPageDescription,
         site_url: site_url,
         feed_url: `${site_url}/rss.xml`,
-        image_url: `${site_url}/logo.png`,
+        image_url: '../public/images/logo.svg',
         pubDate: new Date(),
     };
 
     const feed = new RSS(feedOptions);
 
-    allPosts.map((post) => {
+    response.posts.nodes.forEach((post) => {
         feed.item({
-         title: post.title,
-         description: post.excerpt,
-         url: `${site_url}/blog/${post.slug}`,
-         date: post.date,
+            title: post.title,
+            description: post.excerpt,
+            url: `${site_url}/blog/${post.slug}`,
+            date: post.date,
+            author: post.author.node.name,
+            custom_elements: [
+                {'categories': post.categories.edges?.map(a => a.node.name)},
+            ] 
         });
        });
     
