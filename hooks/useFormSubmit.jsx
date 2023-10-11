b409a7946426cef4350420d0e43010141a8c0c12
Lee Wise <theleewise@users.noreply.github.com>
2023-10-11 14:26:28 -0500

    finish form hook

--- hooks/useFormSubmit.jsx
+++ hooks/useFormSubmit.jsx
@@ -8,21 +8,14 @@ export default function useFormSubmit() {
   const submit = async (url, data) => {
     try {
       setIsLoading(true);
-      const { ok, status, statusText, body } = await fetch(url, {
+      const { ok, status, statusText } = await fetch(url, {
         method: 'POST',
         body: JSON.stringify(data),
       });
-      const responseError = await body.json();
-      // #TODO - PICKUPHERE
-
-      if (!ok) {
-        if (responseError) throw new Error(responseError);
-        throw new Error(`${status}: ${statusText}`);
-      }
-        
+      if (!ok) throw `${status}: ${statusText}`;
       setHasSubmited(true);
     } catch (error) {
-      setError(error.message);
+      setError(error);
     } finally {
       setIsLoading(false);
     }
