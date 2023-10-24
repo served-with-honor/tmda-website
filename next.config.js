61db814eda310a3305cb49c78dfe8a25a54b3f34
Lee Wise <theleewise@users.noreply.github.com>
2023-10-24 12:36:09 -0500

    add default language to config

--- next.config.js
+++ next.config.js
@@ -5,6 +5,10 @@ const nextConfig = {
     SANITY_PROJECT_ID: process.env.SANITY_PROJECT_ID,
     SANITY_DATASET: process.env.SANITY_DATASET,
   },
+  i18n: {
+    locales: ["en"],
+    defaultLocale: "en",
+  },
   async redirects() {
     return [
       // Pages
