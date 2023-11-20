a470a620280c1b0a2fcd868a2494498f1e09f95e
Lee Wise <theleewise@users.noreply.github.com>
2023-11-20 15:49:24 -0600

    oops, left this out of previous commit

--- src/WPBlocks.js
+++ src/WPBlocks.js
@@ -112,6 +112,22 @@ export const replaceContent = (element) => {
 	return element;
 }
 
+export const WPHeading = ({ name, children, attribs }) => {
+	const { id } = attribs || {};
+	const component = name === 'h1' ? 'h2' : name;
+	const sizeNum = parseInt(name.replace('h', ''));
+	const variant = `h${sizeNum + 2 > 6 ? 6 : sizeNum + 2}`;
+	return <Typography
+		variant={variant}
+		component={component}
+		color='secondary.main'
+		my={6}
+		id={id}
+	>
+		{renderElementContents(children)}
+	</Typography>;
+}
+
 const replaceButtons = ({ children, attribs }) => {
 	const { class: classes } = attribs || {};
 	if (!children) return;
