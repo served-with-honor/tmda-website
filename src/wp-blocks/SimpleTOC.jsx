b4fdd115bf5d6d432cfd9186665459591ffc7162
Lee Wise <theleewise@users.noreply.github.com>
2023-12-06 15:41:02 -0600

    overhaul

--- src/wp-blocks/SimpleTOC.jsx
+++ src/wp-blocks/SimpleTOC.jsx
@@ -1,22 +1,68 @@
 import { domToReact } from 'html-react-parser';
 import Typography from '@mui/material/Typography'
-import WPBlockList from './WPBlockList';
+import Box from '@mui/material/Box';
+import Link from '../Link';
+import { isEmptyText } from './generics';
 
 export default function SimpleTOC(element) {
-	const { name, attribs, children } = element;
+	const { attribs, children } = element;
 	const { class: classes } = attribs || {};
 	
-	if (classes?.includes('simpletoc-title'))
-		return (
-			<Typography
-				variant='h6'
-				component='h2'
-				color='secondary.main'
-				gutterBottom
-			>
-				{domToReact(children)}
-			</Typography>
-		);
+	const isHeading = classes?.includes('simpletoc-title');
+	const isList = classes?.includes('simpletoc-list');
 	
-	if (classes?.includes('simpletoc-list')) return WPBlockList(element);
+	if (isEmptyText(element)) return;
+
+	if (isHeading) return (
+		<Typography
+			variant='h6'
+			component='h2'
+			color='secondary.main'
+			gutterBottom
+		>
+			{domToReact(children)}
+		</Typography>
+	);
+	
+	if (isList) return (
+		<Box sx={{
+			lineHeight: 1.1,
+			fontSize: 'body2.fontSize',
+			'ol': { pl: 2, },
+			'li': { my: 2, },
+			'a': {
+				display: 'block',
+				textDecoration: 'none',
+				':hover': { textDecoration: 'underline' },
+			},
+		}}>
+			<ol>
+				{domToReact(children, { replace: renderListItem })}
+			</ol>
+		</Box>
+	);
+
+	return domToReact(element);
+}
+
+const renderListItem = (element) => {
+	if(isEmptyText(element)) return;
+	
+	return <li>
+		{domToReact(element.children, { replace: renderContent })}
+	</li>;
+}
+
+const renderContent = (element) => {
+	const { name, children, attribs } = element;
+	
+	if (isEmptyText(element)) return;
+	
+	if (name === 'ul') return (
+		<ol>{domToReact(children, { replace: renderListItem })}</ol>
+	);
+
+	if (name === 'a' && children) return <Link {...attribs}>{domToReact(children)}</Link>
+	
+	return domToReact(element);
 }
