8cbdd875ba7b4d119f28341f14776f1368c5efff
Lee Wise <theleewise@users.noreply.github.com>
2024-05-14 11:57:19 -0500

    Hotfix/broken-wp-blocks-table (#74)
    * add missing import
    
    * add support for cell span
    
    * add support for cell alignment
    
    * refactor
    
    * simplify

--- frontend/src/wp-blocks/WPBlockTable.js
+++ frontend/src/wp-blocks/WPBlockTable.js
@@ -1,3 +1,4 @@
+import { domToReact } from 'html-react-parser';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
@@ -6,59 +7,51 @@ import TableHead from '@mui/material/TableHead';
 import TableFooter from '@mui/material/TableFooter';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import { getTextAlignment } from './generics';
 
 export default function WPBlockTable({ children }) {
+	// This is because the table is wrapped in a figure tag
 	const table = children.find(({ name }) => name === 'table');
-	const tableHeader = table?.children.find(({ name }) => name === 'thead')?.children[0];
-	const tableBody = table?.children.find(({ name }) => name === 'tbody');
-	const tableFooter = table?.children.find(({ name }) => name === 'tfoot')?.children[0];
-	const caption = children.find(({ name }) => name === 'figcaption');
-	
+
 	return (
     <TableContainer component={Paper} sx={{ my: 6 }}>
 			<Table>
-
-				{caption ? (
-					<caption>{domToReact(caption.children)}</caption>
-				) : null}
-
-				{tableHeader ? (
-					<TableHead>
-						<TableRow>
-							{domToReact(tableHeader.children, {
-								replace: (element) => {
-									return <TableCell>{domToReact(element.children)}</TableCell>
-								}
-							})}
-						</TableRow>
-					</TableHead>
-				) : null}
-
-			{tableBody ? (
-				<TableBody>
-					{domToReact(tableBody.children, { replace: (row) => (
-						<TableRow>
-							{domToReact(row.children, { replace: (cell) => (
-								<TableCell>{domToReact(cell.children)}</TableCell>
-							)})}
-						</TableRow>
-					)})}
-				</TableBody>
-			): null}
-
-			{tableFooter ? (
-				<TableFooter>
-					<TableRow>
-						{domToReact(tableFooter.children, {
-							replace: (element) => {
-								return <TableCell>{domToReact(element.children)}</TableCell>
-							}
-						})}
-					</TableRow>
-				</TableFooter>
-			) : null}
-
+				{domToReact(table.children, {
+					replace: (element) => {
+						const { name, children } = element;
+						
+						if (name === 'thead') 
+							return <TableHead>{domToReact(children, { replace: replaceRow })}</TableHead>
+						
+						if (name === 'tbody') 
+							return <TableBody>{domToReact(children, { replace: replaceRow })}</TableBody>
+						
+						if (name === 'tfoot') 
+							return <TableFooter>{domToReact(children, { replace: replaceRow })}</TableFooter>
+						
+						return element;
+					}
+				})}
 			</Table>
 		</TableContainer>
 	);
+}
+
+const replaceRow = ({ children }) => {
+	return (
+		<TableRow>
+			{domToReact(children, { replace: replaceCell })}
+		</TableRow>
+	);
+}
+
+const replaceCell = ({ attribs, children }) => {
+	const align = getTextAlignment(attribs.class);
+	const colspan = attribs.colspan || 1;
+
+	return (
+		<TableCell align={align} colSpan={colspan}>
+			{domToReact(children)}
+		</TableCell>
+	)
 }
