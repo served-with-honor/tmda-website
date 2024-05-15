e9a5691e15b52138bf8d4ee558dd17dfeed3c8fe
Tim Tran <tim-swh@tims-air.lan>
2024-05-15 18:48:01 -0500

    updates with Lee to reviewer component

--- frontend/components/ReviewerBlock.jsx
+++ frontend/components/ReviewerBlock.jsx
@@ -1,12 +1,9 @@
 import { Card, Grid, Typography, Box, Avatar, Stack } from "@mui/material";
 
-export default function ReviewerBlock() {
+export default function ReviewerBlock({ date, name, imageUrl, bio }) {
   return (
     <Card
       sx={{
-        maxWidth: 500,
-        minHeight: 450,
-        ml: 20,
         border: 1,
         borderColor: "primary.main",
         borderWidth: 2,
@@ -14,61 +11,51 @@ export default function ReviewerBlock() {
     >
       <Box
         sx={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          pt: 2,
-          ml: 1,
+          ".MuiGrid-item": {
+            p: 2,
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
+            justifyContent: "center",
+          },
         }}
       >
-        <Grid container spacing={2} sx={{ borderBottom: 1 }}>
+        <Grid container sx={{ borderBottom: 1, borderColor: "primary.main" }}>
           <Grid
             item
             xs={7}
-            sx={{ borderRight: 1, backgroundColor: "primary.main" }}
+            sx={{
+              backgroundColor: "primary.main",
+            }}
           >
-            <Typography variant="h5" component="p">
-              Last Date Modified
-            </Typography>
+            <Typography variant="subtitle2">Last Date Modified</Typography>
           </Grid>
           <Grid item xs={5}>
-            <Typography variant="h5" component="p">
-              April 2, 2024
+            <Typography variant="subtitle2">{date}</Typography>
+          </Grid>
+        </Grid>
+      </Box>
+      <Box sx={{ p: 3 }}>
+        <Grid container spacing={3} sx={{ alignItems: "center" }}>
+          <Grid item>
+            <Avatar alt={name} src={imageUrl} sx={{ width: 64, height: 64 }} />
+          </Grid>
+          <Grid item>
+            <Typography variant="body1" sx={{ borderBottom: 1 }}>
+              Content Reviewed By:
+            </Typography>
+            <Typography variant="h6" component="p">
+              {name}
             </Typography>
           </Grid>
         </Grid>
-      </Box>
-      <Box>
-        <Grid container spacing={20}>
-          <Grid item xs={3}>
-            <Avatar
-              alt="Reviewer"
-              src="/static/images/avatar/1.jpg"
-              sx={{ width: 125, height: 125, ml: 3, mt: 2 }}
-            />
-          </Grid>
-          <Grid item xs={9}>
-            <Stack spacing={2} sx={{ mt: 5, ml: 3 }}>
-              <Typography variant="body1" sx={{ textDecoration: "underline" }}>
-                Content Reviewed By:
-              </Typography>
-              <Typography variant="h6" component="p" sx={{ ml: 3, mt: 2 }}>
-                {" "}
-                Name of Reviewer
-              </Typography>
-            </Stack>
-          </Grid>
-        </Grid>
-      </Box>
-      <Box>
-        <Typography variant="body1" sx={{ mt: 3, mx: 2 }}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod
-          justo nec mauris tincidunt, at lacinia nunc tincidunt. Nullam Lorem
-          ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod justo
-          nec mauris tincidunt, at lacinia nunc tincidunt. Nullam Lorem ipsum
-          dolor sit amet, consectetur adipiscing elit. Sed euismod justo nec
-          mauris tincidunt, at lacinia nunc tincidunt. Nullam
-        </Typography>
+        <Box>
+          {bio ? (
+            <Typography variant="body1" sx={{ mt: 3 }}>
+              {bio}
+            </Typography>
+          ) : null}
+        </Box>
       </Box>
     </Card>
   );
