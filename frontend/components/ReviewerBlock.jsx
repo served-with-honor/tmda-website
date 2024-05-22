cda79dea53a425142d6b90a9bcda5a67614cace6
Tim Tran <tim-swh@tims-air.lan>
2024-05-22 16:26:17 -0500

    updates reviewer box from EWebbs recommendations

--- frontend/components/ReviewerBlock.jsx
+++ frontend/components/ReviewerBlock.jsx
@@ -8,47 +8,28 @@ export default function ReviewerBlock({ date, name, imageUrl, bio }) {
   return (
     <Card
       sx={{
-        border: 1,
-        borderColor: "primary.main",
-        borderWidth: 2,
+        backgroundColor: "secondary.main",
+        color: "secondary.contrastText",
       }}
     >
-      <Box
-        sx={{
-          ".MuiGrid-item": {
-            p: 2,
-            display: "flex",
-            flexDirection: "row",
-            alignItems: "center",
-            justifyContent: "center",
-          },
-        }}
-      >
-        <Grid container sx={{ borderBottom: 1, borderColor: "primary.main" }}>
-          <Grid
-            item
-            xs={7}
-            sx={{
-              backgroundColor: "primary.main",
-            }}
-          >
-            <Typography variant="subtitle2">Last Date Modified</Typography>
-          </Grid>
-          <Grid item xs={5}>
-            <Typography variant="subtitle2">
-              {new Date(date).toLocaleDateString()}
-            </Typography>
-          </Grid>
-        </Grid>
-      </Box>
       <Box sx={{ p: 3 }}>
         <Grid container spacing={3} sx={{ alignItems: "center" }}>
           <Grid item>
-            <Avatar alt={name} src={imageUrl} sx={{ width: 64, height: 64 }} />
+            <Avatar
+              alt={name}
+              src={imageUrl}
+              sx={{
+                width: 64,
+                height: 64,
+                borderColor: "secondary.contrast.text",
+                border: "solid",
+                borderWidth: 2,
+              }}
+            />
           </Grid>
           <Grid item>
             <Typography variant="body1" sx={{ borderBottom: 1 }}>
-              Content Reviewed By:
+              Reviewed By:
             </Typography>
             <Typography variant="h6" component="p">
               {name}
