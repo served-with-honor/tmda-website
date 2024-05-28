183332908cb5843d82f5d516bb7a4b81079543ee
Tim Tran <tim-swh@tims-air.lan>
2024-05-28 10:42:59 -0500

    reverts changes from staging merge

--- frontend/pages/consultation-call.js
+++ frontend/pages/consultation-call.js
@@ -1,36 +1,53 @@
-import Page from '../components/Page';
-import Box from '@mui/material/Box';
-import Container from '@mui/material/Container';
-import Typography from '@mui/material/Typography';
-import texture01 from '../public/images/texture-01.jpg';
-import constants from '../src/constants';
-import { InlineWidget } from 'react-calendly';
+import Page from "../components/Page";
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+import texture01 from "../public/images/texture-01.jpg";
+import constants from "../src/constants";
+import { InlineWidget } from "react-calendly";
 
 export default function ScheduleCallPage({ title }) {
-    return (
-        <Page title={title} noindex>
-            <Box sx={{
-                py: 20,
-                background: `url(${texture01.src}) center / cover no-repeat`,
-            }}>
-                <Container maxWidth='md' sx={{ textAlign: 'center'}}>
-                    <Typography variant='h2' component='h1' color='primary'> Schedule A Consultation Call</Typography>
-                    <Typography variant='lead' component='p' sx={{ mb: 3 }}>Learn how our medical evidence experts can help you strengthen your VA disability claim.</Typography>
-                    <Typography variant='body1'>We understand the VA disability claims process can be confusing—and that includes knowing what to include when you submit your VA claim. The good news is, you don't have to figure it out alone. Get personalized guidance with a <strong>FREE</strong> Telemedica Consultation Call by scheduling below!</Typography>
-                </Container>
-            </Box>
-            <Box sx={{ pb:12, mt: {xs: -8, lg: -14} }}>
-                <InlineWidget styles={{height: '735px'}} url={constants.calendly.consultationCall} />
-            </Box>
-        </Page>
-    )
+  return (
+    <Page title={title}>
+      <Box
+        sx={{
+          py: 20,
+          background: `url(${texture01.src}) center / cover no-repeat`,
+        }}
+      >
+        <Container maxWidth="md" sx={{ textAlign: "center" }}>
+          <Typography variant="h2" component="h1" color="primary">
+            {" "}
+            Schedule A Consultation Call
+          </Typography>
+          <Typography variant="lead" component="p" sx={{ mb: 3 }}>
+            Learn how our medical evidence experts can help you strengthen your
+            VA disability claim.
+          </Typography>
+          <Typography variant="body1">
+            We understand the VA disability claims process can be confusing—and
+            that includes knowing what to include when you submit your VA claim.
+            The good news is, you don't have to figure it out alone. Get
+            personalized guidance with a <strong>FREE</strong> Telemedica
+            Consultation Call by scheduling below!
+          </Typography>
+        </Container>
+      </Box>
+      <Box sx={{ pb: 12, mt: { xs: -8, lg: -14 } }}>
+        <InlineWidget
+          styles={{ height: "735px" }}
+          url={constants.calendly.consultationCall}
+        />
+      </Box>
+    </Page>
+  );
 }
 
 export async function getStaticProps() {
-    const title = 'Schedule a Call';
-    return {
-        props: {
-            title,
-        }
-    }
+  const title = "Schedule a Call";
+  return {
+    props: {
+      title,
+    },
+  };
 }
