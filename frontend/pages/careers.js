2e54fe9016eb4aa69c8d8e75a43fe0a60469e385
Tim Tran <tim-swh@tims-air.lan>
2024-07-17 10:54:31 -0500

    replaces with careers portal link

--- frontend/pages/careers.js
+++ frontend/pages/careers.js
@@ -29,7 +29,7 @@ export default function CareersPage({ title, description }) {
 							lineHeight: 1,
 							mb: 5,
 						}}>100% remote working opportunities.</Typography>
-						<Button href={constants.externalLinks.jobs} target='_blank' rel='noopener noreferer' variant='contained' size='lg' sx={{ fontWeight: 700, px: 3, py: 1 }}>View Job Postings</Button>
+						<Button href="/careers-portal/index.html" target='_blank' rel='noopener noreferer' variant='contained' size='lg' sx={{ fontWeight: 700, px: 3, py: 1 }}>View Job Postings</Button>
 					</Box>
 				</Container>
 			</Box>
